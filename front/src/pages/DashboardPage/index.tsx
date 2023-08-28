47d468cf00705d6ef7875202639b9f6243ae6df0
saviocosta01 <savio.costa.silva@gmail.com>
2023-08-28 19:13:19 -0300

    feat: creating page structure

---
+++ front/src/pages/DashboardPage/index.tsx
@@ -0,0 +1,7 @@
+
+
+export const DashboardPage = () => {
+    return(
+        <h2>dashboard</h2>
+    )
+}
---
+++ front/src/pages/LoginPage/index.tsx
@@ -0,0 +1,7 @@
+
+
+export const LoginPage = () => {
+    return(
+        <h2>login</h2>
+    )
+}
